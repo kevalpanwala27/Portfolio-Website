b7e87374203f856680fd7ed37fef71328839c00f
Keval Panwala <kevalpanwala@Kevals-Air.mshome.net>
2024-01-02 19:47:07 +0530

    finish about and project page

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,21 @@
-// import About from "@/components/about";
+import About from "@/components/about";
 // import Contact from "@/components/contact";
 // import Experience from "@/components/experience";
 import Intro from "@/components/intro";
-// import Projects from "@/components/projects";
-// import SectionDivider from "@/components/section-divider";
+import Projects from "@/components/projects";
+import SectionDivider from "@/components/section-divider";
 // import Skills from "@/components/skills";
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center px-4">
       <Intro />
-      {/* <SectionDivider />
+      <SectionDivider />
       <About />
       <Projects />
-      <Skills />
+      {/* <Skills />
       <Experience />
-      <Contact /> */}
+      <Contact />  */}
     </main>
   );
 }
--- components/intro.tsx
+++ components/intro.tsx
@@ -67,7 +67,7 @@ export default function Intro() {
         <span className="font-bold">
           Full-stack developer, Flutter developer, DevOps Engineer
         </span>{" "}
-        with <span className="font-bold">little bit of</span> of experience. I
+        with <span className="font-bold">little bit</span> of experience. I
         enjoy building <span className="italic">sites & apps</span>. My focus is{" "}
         <span className="underline">Frontend, Backend and DevOps</span>.
       </motion.h1>
